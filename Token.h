b5cf859876e0caebdd5544e0f2969a2ec8b88195
Andenor <rowebot98@gmail.com>
2021-09-22 22:29:32 -0600

    Initial commit

---
+++ Token.h
@@ -0,0 +1,39 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+#include <string>
+
+enum class TokenType {
+    COLON,
+    COLON_DASH,
+    COMMA,
+    PERIOD,
+    Q_MARK,
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    MULTIPLY,
+    ADD,
+    SCHEMES,
+    FACTS,
+    RULES,
+    QUERIES,
+    ID,
+    STRING,
+    COMMENT,
+    END_OF_FILE,
+    UNDEFINED
+};
+
+class Token
+{
+private:
+    TokenType type;
+    std::string description;
+    int line;
+
+public:
+    Token(TokenType type, std::string description, int line);
+    std::string ToString() const;
+};
+
+#endif // TOKEN_H
+
